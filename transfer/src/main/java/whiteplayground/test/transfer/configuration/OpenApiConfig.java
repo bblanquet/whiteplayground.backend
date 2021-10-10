fe094825af88832411f59cbaa3ad179b0562f876
bertrand Blanquet <bertrand.blanquet6@gmail.com>
2021-10-10 21:43:31 +0900

    BACK java fix swagger issue on PRD environment.

--- transfer/src/main/java/whiteplayground/test/transfer/configuration/OpenApiConfig.java
+++ transfer/src/main/java/whiteplayground/test/transfer/configuration/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package whiteplayground.test.transfer.configuration;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,4 +18,9 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer"
 )
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI(@Value("${url.value}") String appVersion) {
+        return new OpenAPI()
+                .addServersItem(new Server().url(appVersion));
+    }
 }
